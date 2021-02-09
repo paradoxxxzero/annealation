fb114f59ae99bc4df2b4ab139bf8385e55e0e9f3
Florian Mounier <florian.mounier@kozea.fr>
2021-02-09 12:43:36 +0100

    Add gui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import { GUI } from 'dat.gui'
 import {
-  AmbientLight,
   BufferAttribute,
   BufferGeometry,
   Color,
-  FogExp2,
+  NoToneMapping,
   PerspectiveCamera,
   Points,
   ReinhardToneMapping,
@@ -20,6 +20,7 @@ import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
 import fragmentShader from './fragmentShader.glsl'
 import vertexShader from './vertexShader.glsl'
+import presets from './presets'
 
 const params = {
   // Effects
@@ -48,7 +49,6 @@ const params = {
 const renderer = new WebGLRenderer({ antialias: true })
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
-
 renderer.toneMapping = ReinhardToneMapping
 document.body.appendChild(renderer.domElement)
 
@@ -73,10 +73,10 @@ const composer = new EffectComposer(renderer)
 const renderScene = new RenderPass(scene, camera)
 composer.addPass(renderScene)
 
-const afterimagePass = new AfterimagePass()
-afterimagePass.uniforms.damp.value = params.afterImageDamp
+const afterImagePass = new AfterimagePass()
+afterImagePass.uniforms.damp.value = params.afterImageDamp
 if (params.afterImage) {
-  composer.addPass(afterimagePass)
+  composer.addPass(afterImagePass)
 }
 
 const bloomPass = new UnrealBloomPass(
@@ -111,24 +111,6 @@ function render() {
   update(simulate())
   composer.render()
 }
-
-let orbs = new Array(params.initParticleSize).fill().map(() => ({
-  color: new Color().setHSL(Math.random(), params.saturation, params.luminance),
-  // weight 10^31 kg
-  weight: Math.random() * params.initWeightRange,
-  // distance 10^16 m
-  position: new Vector3(
-    params.initPositionRange / 2 - Math.random() * params.initPositionRange,
-    params.initPositionRange / 2 - Math.random() * params.initPositionRange,
-    params.initPositionRange / 2 - Math.random() * params.initPositionRange
-  ),
-  speed: new Vector3(
-    params.initSpeedRange / 2 - Math.random() * params.initSpeedRange,
-    params.initSpeedRange / 2 - Math.random() * params.initSpeedRange,
-    params.initSpeedRange / 2 - Math.random() * params.initSpeedRange
-  ),
-  acceleration: new Vector3(),
-}))
 
 // let orbs = [
 //   {
@@ -171,9 +153,35 @@ function collide([orb, otherOrb]) {
     acceleration: new Vector3(),
   }
 }
-const geometry = new BufferGeometry()
+let geometry, orbs
+
+function initOrbs() {
+  orbs = new Array(params.initParticleSize).fill().map(() => ({
+    color: new Color().setHSL(
+      Math.random(),
+      params.saturation,
+      params.luminance
+    ),
+    // weight 10^31 kg
+    weight: Math.random() * params.initWeightRange,
+    // distance 10^16 m
+    position: new Vector3(
+      params.initPositionRange / 2 - Math.random() * params.initPositionRange,
+      params.initPositionRange / 2 - Math.random() * params.initPositionRange,
+      params.initPositionRange / 2 - Math.random() * params.initPositionRange
+    ),
+    speed: new Vector3(
+      params.initSpeedRange / 2 - Math.random() * params.initSpeedRange,
+      params.initSpeedRange / 2 - Math.random() * params.initSpeedRange,
+      params.initSpeedRange / 2 - Math.random() * params.initSpeedRange
+    ),
+    acceleration: new Vector3(),
+  }))
+}
 
 function init() {
+  initOrbs()
+  geometry = new BufferGeometry()
   const positions = new Float32Array(orbs.length * 3)
   const scales = new Float32Array(orbs.length)
   const colors = new Float32Array(orbs.length * 3)
@@ -200,7 +208,6 @@ function init() {
 
   const particles = new Points(geometry, material)
   scene.add(particles)
-  requestAnimationFrame(animate)
 }
 
 const u = new Vector3()
@@ -294,4 +301,78 @@ function update(updated) {
   geometry.attributes.color.needsUpdate = updated
 }
 
+const gui = new GUI({
+  load: presets,
+  preset: decodeURIComponent(location.hash.replace(/^#/, '')) || 'Tesseract',
+})
+const fx = gui.addFolder('Render fx')
+fx.add(params, 'bloom').onChange(on => {
+  if (on && !composer.passes.includes(bloomPass)) {
+    renderer.toneMapping = ReinhardToneMapping
+    composer.addPass(bloomPass)
+  }
+  if (!on && composer.passes.includes(bloomPass)) {
+    renderer.toneMapping = NoToneMapping
+    composer.removePass(bloomPass)
+  }
+})
+fx.add(params, 'bloomStrength', 0, 3).onChange(v => (bloomPass.strength = v))
+fx.add(params, 'bloomRadius', 0, 1).onChange(v => (bloomPass.radius = v))
+fx.add(params, 'bloomThreshold', 0, 1).onChange(v => (bloomPass.threshold = v))
+fx.add(params, 'bloomExposure', 0.001, 128).onChange(
+  v => (renderer.toneMappingExposure = v)
+)
+fx.add(params, 'afterImage').onChange(on => {
+  if (on && !composer.passes.includes(afterImagePass)) {
+    if (composer.passes.includes(bloomPass)) {
+      composer.insertPass(afterImagePass, composer.passes.indexOf(bloomPass))
+    } else {
+      composer.addPass(afterImagePass)
+    }
+  }
+  if (!on && composer.passes.includes(afterImagePass)) {
+    composer.removePass(afterImagePass)
+  }
+})
+fx.add(params, 'afterImageDamp', 0, 1).onChange(
+  v => (afterImagePass.uniforms.damp.value = v)
+)
+const initParams = gui.addFolder('Initialization')
+initParams.add(params, 'initParticleSize', 0, 5000, 1)
+initParams.add(params, 'initPositionRange', 0, 5000, 1)
+initParams.add(params, 'initSpeedRange', 0, 100)
+initParams.add(params, 'initWeightRange', 0, 100)
+initParams.add(params, 'saturation', 0, 1)
+initParams.add(params, 'luminance', 0, 1)
+initParams.add(
+  {
+    restart: () => {
+      scene.clear()
+      init()
+    },
+  },
+  'restart'
+)
+
+initParams.open()
+const simulation = gui.addFolder('Simulation')
+simulation.add(params, 'gravitationalConstant', 0.0, 25.0, 0.1)
+simulation.add(params, 'simulationSpeed', 0.0, 10.0, 0.01)
+simulation.add(params, 'collisionBase', 0, 1000, 1)
+simulation.add(params, 'collisionScale', 0, 1000, 1)
+simulation.add(params, 'escapeDistance', 0, 100000, 1)
+simulation.open()
+
+gui.remember(params)
+gui.revert()
+gui.__preset_select.addEventListener('change', ({ target: { value } }) => {
+  location.hash = `#${encodeURIComponent(value)}`
+})
+window.addEventListener('hashchange', () => {
+  gui.preset =
+    decodeURIComponent(location.hash.replace(/^#/, '')) || 'Tesseract'
+  gui.revert()
+})
+
 init()
+requestAnimationFrame(animate)
